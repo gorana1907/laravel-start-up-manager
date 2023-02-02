d97f45b7e11cc00209a7ccd51555c0166256ef9e
Gorana <greenkoala1907@gmail.com>
2023-02-03 00:31:51 +0100

    Autentifikacija preko Sanctuma i grupna ruta

--- start-up_manager/routes/api.php
+++ start-up_manager/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvestitorController;
 use App\Http\Controllers\InvesticijaController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +16,20 @@ use App\Http\Controllers\InvesticijaController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [AuthController::class, 'register']);
+
+Route::post('login', [AuthController::class, 'login']);
+
 Route::get('investitor', [InvestitorController::class, 'index']);
 
 Route::get('investitor/{investitor}', [InvestitorController::class, 'show']);
 
-Route::put('investitor/{investitor}', [InvestitorController::class, 'update']);
-
-Route::delete('investitor/{investitor}', [InvestitorController::class, 'destroy']);
-
 Route::get('investicija', [InvesticijaController::class, 'index']);
 
-Route::delete('investicija/{investicija}', [InvesticijaController::class, 'destroy']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::put('investitor/{investitor}', [InvestitorController::class, 'update']);
+    Route::delete('investitor/{investitor}', [InvestitorController::class, 'destroy']);
+    Route::delete('investicija/{investicija}', [InvesticijaController::class, 'destroy']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
